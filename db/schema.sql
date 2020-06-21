35a3179cae601f9f762713c4e06267f05429387b
jpd61 <jpd@dewoodyoil.com>
2020-06-21 17:21:36 -0500

    add not null to sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,20 +4,20 @@ USE employees;
 
 CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    department_name VARCHAR(30)
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary DECIMAL(10, 2),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
     department_id INTEGER
 );
 
 CREATE TABLE employee(
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER
 );
