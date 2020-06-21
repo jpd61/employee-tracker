10cb5a583b63b8ad9159eb8bc3f10b04a2a2ef57
jpd61 <jpd@dewoodyoil.com>
2020-06-21 16:04:55 -0500

    refactoring

--- javascript/validate.js
+++ javascript/validate.js
@@ -2,11 +2,11 @@ const validator = require('validator');
 
 const validate = {
   validateString(str) {
-    return str !== '' || 'Please enter a valid response.';
+    return str !== '' || 'Please enter a valid response!';
   },
   validateSalary(num) {
     if (validator.isDecimal(num)) return true;
-    return 'Please enter a valid salary.';
+    return 'Please enter a valid salary!';
   },
   isSame(str1, str2) {
     if (str1 === str2) return true;
